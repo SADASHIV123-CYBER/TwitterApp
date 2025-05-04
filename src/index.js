717949a9b015a4d61be045e986b3e5bbdb3af2fc
sadashiv <sadashivkale604@gmail.com>
2025-05-04 21:29:50 +0530

    understanded meddleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
-app.get('/ping', (req, res) => {
+app.use(morgan('combined'));
+
+function mid1(req, res, next) {
+    console.log("mid1");
+    next();
+}
+
+function mid2(req, res, next) {
+    console.log("mid2");
+    next();
+}
+
+function mid3(req, res, next) {
+    console.log("mid3");
+    next();
+};
+
+const middleware = [mid1, mid2, mid3]
+
+function commonMiddleware(req, res, next) {
+    console.log("commonMiddleware");
+    next();
+}
+
+app.use(commonMiddleware);
+
+app.get('/ping', middleware, (req, res) => {
     return res.json({
         message: "pong"
     })
-})
+});
 
-app.post('/hello', (req, res) => {
+app.post('/hello', [mid1, mid2], (req, res) => {
     return res.json({
         message: "world"
     })
